dfdd22343b04b87ef632ecb1bebe9a939798bdf3
millymike <millymike599@gmail.com>
2023-09-07 16:56:27 +0100

    clean up

--- MsOrleans/Program.cs
+++ MsOrleans/Program.cs
@@ -2,24 +2,18 @@ using MsOrleans.Features;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
 builder.Host.UseOrleans(siloBuilder =>
 {
     siloBuilder.UseLocalhostClustering();
 });
 
 builder.Services.AddControllers();
-
 builder.Services.AddTransient<IHelloWorldGrain, HelloWorldGrain>();
-
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
@@ -27,9 +21,6 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
-
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
